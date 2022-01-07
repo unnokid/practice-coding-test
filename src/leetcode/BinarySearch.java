48efbf413ba6f9e40a727c77a9ab8207b837e715
unnokid <kimki0609@gmail.com>
2022-01-07 15:08:48 +0900

    Update leetcode static Solution

--- src/leetcode/BinarySearch.java
+++ src/leetcode/BinarySearch.java
@@ -25,58 +25,61 @@ public class BinarySearch {
         nums = new int[]{2,5};
         target =2;
         System.out.println(solution.search(nums,target));
+
+
     }
 
-}
-class Solution {
-    public int search(int[] nums, int target) {
-        int answer = -1;
-        //정렬된 배열이 주어짐
-        //target이 존재한다면 그 인덱스 return
-        //존재하지 않는다면 -1 return
-        //반드시 O(log n) 알고리즘을 사용해라
-        int beforeStart =0;
-        int beforeEnd =0;
+    static class Solution {
+        public int search(int[] nums, int target) {
+            int answer = -1;
+            //정렬된 배열이 주어짐
+            //target이 존재한다면 그 인덱스 return
+            //존재하지 않는다면 -1 return
+            //반드시 O(log n) 알고리즘을 사용해라
+            int beforeStart =0;
+            int beforeEnd =0;
 
-        int start =0;
-        int end = nums.length;
-        int length = end - start;
+            int start =0;
+            int end = nums.length;
+            int length = end - start;
 
-        if(nums.length == 1){
-            if(nums[0] == target){
-                return 0;
+            if(nums.length == 1){
+                if(nums[0] == target){
+                    return 0;
+                }
+                else{
+                    return -1;
+                }
             }
-            else{
-                return -1;
+
+            length/=2;
+            while(true){
+                if(start == beforeStart && end == beforeEnd ){
+                    break;
+                }
+
+                if(nums[length] == target){
+                    answer = length;
+                    break;
+                }
+                else if(nums[length] > target){
+                    //인덱스가 내려가야됨
+                    beforeEnd = end;
+                    beforeStart = start;
+                    end = length;
+                    length = (length+start)/2;
+
+                }
+                else{
+                    //인덱스가 올라가야됨
+                    beforeEnd = end;
+                    beforeStart = start;
+                    start = length;
+                    length = (length+end)/2;
+                }
             }
+            return answer;
         }
-
-        length/=2;
-        while(true){
-            if(start == beforeStart && end == beforeEnd ){
-                break;
-            }
-
-            if(nums[length] == target){
-                answer = length;
-                break;
-            }
-            else if(nums[length] > target){
-                //인덱스가 내려가야됨
-                beforeEnd = end;
-                beforeStart = start;
-                end = length;
-                length = (length+start)/2;
-
-            }
-            else{
-                //인덱스가 올라가야됨
-                beforeEnd = end;
-                beforeStart = start;
-                start = length;
-                length = (length+end)/2;
-            }
-        }
-        return answer;
     }
+
 }
---
+++ src/leetcode/SearchInsertPosition.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+
+    static class Solution {
+        public int searchInsert(int[] nums, int target) {
+
+            //정렬된 각각 다른 배열과 target 을 준다
+            //찾을수 있으면 그 배열의 인덱스를 return
+            //찾을 수 없으면 그 숫자가 들어갈 수 있는 인덱스를 찾아서 return;
+
+            if(target < nums[0]){
+                return 0;
+            }
+
+            if(target > nums[nums.length-1]){
+                return nums.length;
+            }
+
+            int start =0;
+            int end = nums.length-1;
+            int index;
+
+            while(start <= end){
+                index = (start+end)/2;
+
+                //찾았으면 바로 끝
+                if(target == nums[index]){
+                    return index;
+                }
+                else if(target > nums[index]){
+                    start = index +1;
+                }
+                else{
+                    end = index -1;
+                }
+            }
+
+            //못찾아서 나왔음
+            if(start > end){
+                return  end+1;
+            }
+
+            //아니면 존재하면 안됨
+            return -1;
+
+        }
+    }
+
+}
+
+
+
