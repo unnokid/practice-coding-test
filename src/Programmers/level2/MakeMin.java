3bf059c0cf09654b56e0f6080e8c000437fdb44f
unnokid <kimki0609@gmail.com>
2022-01-20 21:03:55 +0900

    Update level2 MakeMin.java

---
+++ src/Programmers/level2/MakeMin.java
@@ -0,0 +1,30 @@
+package Programmers.level2;
+
+import java.util.Arrays;
+
+public class MakeMin {
+    public static void main(String[] args) {
+
+    }
+    static class Solution
+    {
+        public int solution(int []A, int []B)
+        {
+            int answer = 0;
+            //길이가 같은 자연수로 이루어진 배열 A B 줌
+            //각각 한개씩 뽑아서 두수를 곱함
+            //최종적으로 누적된 값이 최소가 되도록 하기
+            //한번뽑았으면 다시 뽑을 수 없음
+
+            //크기순으로 정렬후에 최대값과 최소값을 곱한다.
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            for (int i = 0; i < A.length; i++) {
+                answer+= A[i]*B[A.length-1-i];
+            }
+
+            return answer;
+        }
+    }
+}
