7a39192abd37b5f4614dade072d1b41dd0ec81c5
unnokid <kimki0609@gmail.com>
2022-02-09 21:33:02 +0900

    Update leetcode CountNumberofPairsWithAbsoluteDiffenceK.java

--- src/leetcode/CountNumberofPairsWithAbsoluteDiffenceK.java
+++ src/leetcode/CountNumberofPairsWithAbsoluteDiffenceK.java
@@ -14,6 +14,7 @@ public class CountNumberofPairsWithAbsoluteDiffenceK {
     }
     static class Solution {
         public int countKDifference(int[] nums, int k) {
+            // k-Diff 문제 쉬운버전
 
             int answer =0;
             for (int i = 0; i < nums.length-1 ; i++) {
