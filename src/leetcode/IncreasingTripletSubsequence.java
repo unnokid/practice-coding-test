f86c10f6c366f64bc9afaefde9a80cb9059524e7
unnokid <kimki0609@gmail.com>
2022-10-11 11:13:32 +0900

    Update leetcode IncreasingTripletSubsequence.java

---
+++ src/leetcode/IncreasingTripletSubsequence.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class IncreasingTripletSubsequence {
+
+  public static void main(String[] args) {
+
+  }
+  static class Solution {
+    public boolean increasingTriplet(int[] nums) {
+      int first = Integer.MAX_VALUE;
+      int second = Integer.MAX_VALUE;
+
+      for (int i = 0; i <nums.length ; i++) {
+        if(first>= nums[i]){
+          first = nums[i];
+        }else if(second >= nums[i]){
+          second = nums[i];
+        }else{
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
