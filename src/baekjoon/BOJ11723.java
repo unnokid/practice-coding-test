241c8c6e0cf0ef3a93bd7d180365b701cceba2e3
unnokid <kimki0609@gmail.com>
2022-10-25 14:56:30 +0900

    Update baekjoon BOJ11723.java

---
+++ src/baekjoon/BOJ11723.java
@@ -0,0 +1,49 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11723 {
+
+  public static void main(String[] args) throws IOException {
+    //백준 11723 집합
+
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int M = Integer.parseInt(br.readLine());
+
+    int s = 0;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < M; i++) {
+      String[] input = br.readLine().split(" ");
+
+      switch (input[0]) {
+        case "all":
+          s = (1 << 21) - 1;
+          break;
+        case "empty":
+          s = 0;
+          break;
+        default:
+          int x = Integer.parseInt(input[1]);
+          switch (input[0]) {
+            case "add":
+              s |= (1 << x);
+              break;
+            case "remove":
+              s &= ~(1 << x);
+              break;
+            case "check":
+              sb.append((s & (1 << x)) != 0 ? 1 : 0).append('\n');
+              break;
+            case "toggle":
+              s ^= (1 << x);
+              break;
+          }
+      }
+    }
+
+    System.out.println(sb);
+  }
+}
