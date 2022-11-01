008ec58275cd289f3e21390a52700270fddda4b8
unnokid <kimki0609@gmail.com>
2022-11-01 12:01:17 +0900

    Update baekjoon BOJ1158.java

---
+++ src/baekjoon/BOJ1158.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ1158 {
+
+  public static void main(String[] args) throws IOException {
+    // 백준 1158 요세푸스 문제
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] s = br.readLine().split(" ");
+
+    int N = Integer.parseInt(s[0]);
+    int K = Integer.parseInt(s[1]);
+
+    Queue<Integer> q = new LinkedList<>();
+
+    for (int i = 1; i <=N ; i++) {
+      q.add(i);
+    }
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("<");
+
+    while(q.size() != 1){
+      for (int i = 0; i < K-1; i++) {
+        q.add(q.poll());
+      }
+      sb.append(q.poll()).append(", ");
+    }
+
+    sb.append(q.poll()).append(">");
+    System.out.println(sb.toString());
+
+  }
+}
