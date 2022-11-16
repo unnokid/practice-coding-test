fe70a7ac3c34850cd22003fece6d53c374cd4020
unnokid <kimki0609@gmail.com>
2022-11-16 11:54:31 +0900

    Update baekjoon BOJ15652.java

---
+++ src/baekjoon/BOJ15652.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ15652 {
+  static StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) throws IOException {
+    //백준 15652 N과M(4)
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] split = br.readLine().split(" ");
+
+    int N = Integer.parseInt(split[0]);
+    int count = Integer.parseInt(split[1]);
+
+    int[] map = new int[N];
+    for (int i = 0; i < N; i++) {
+      map[i] = i + 1;
+    }
+
+    dfs(map, count, 0, new int[count],0);
+    System.out.println(sb);
+  }
+  static void dfs(int[] map, int count, int choice, int[] list, int start) {
+    if (count == choice) {
+      for (int i = 0; i < list.length; i++) {
+        sb.append(list[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = start; i < map.length; i++) {
+      list[choice] = map[i];
+      dfs(map, count, choice + 1, list,i);
+
+    }
+  }
+}
