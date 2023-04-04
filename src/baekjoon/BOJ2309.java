596a8f4d390fd2a2a8e17418a5941ed7caea131c
unnokid <kimki0609@gmail.com>
2023-04-04 22:10:29 +0900

    Update baekjoon BOJ2309.java

---
+++ src/baekjoon/BOJ2309.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2309 {
+    public static void main(String[] args) throws IOException {
+        //백준 2309 일곱 난쟁이
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
+                    for (int k = 2; k < 9; k++) {
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+
+        }
+
+
+    }
+}
