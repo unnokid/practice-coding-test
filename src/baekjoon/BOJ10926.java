2bc3100b80d06cdf5c27408b1973b133c41e0618
unnokid <kimki0609@gmail.com>
2023-06-15 21:18:40 +0900

    Update baekjoon BOJ10926.java

---
+++ src/baekjoon/BOJ10926.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10926 {
+    public static void main(String[] args) throws IOException {
+        //백준 10926 ??!
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        StringBuilder sb= new StringBuilder();
+        sb.append(s).append("??!");
+        System.out.println(sb);
+    }
+}
