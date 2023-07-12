be3711c89dff5822ff5736fc70c11e31a54f49ac
unnokid <kimki0609@gmail.com>
2023-07-12 20:33:04 +0900

    Update baekjoon BOJ1247.java

---
+++ src/baekjoon/BOJ1247.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class BOJ1247 {
+    public static void main(String[] args) throws IOException {
+        //백준 1247 부호
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        for (int i = 0; i <3 ; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            BigInteger S = new BigInteger("0");
+
+            for (int j = 0; j < N; j++) {
+                BigInteger S2 = new BigInteger(br.readLine());
+                S =S.add(S2);
+            }
+
+            if(S.compareTo(BigInteger.ZERO) == -1){
+                System.out.println("-");
+            }else if(S.compareTo(BigInteger.ZERO) == 1){
+                System.out.println("+");
+            }else{
+                System.out.println(0);
+            }
+        }
+    }
+}
