db8a4c532cad7c2bd241ffa6975a55a18505a469
unnokid <kimki0609@gmail.com>
2023-09-08 21:43:07 +0900

    Update baekjoon BOJ2743.java

---
+++ src/baekjoon/BOJ2743.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+        //백준 2743 단어 길이 재기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+}
