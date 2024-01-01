c525d4ba94c171b648b1410b00a8e722fde2e4cc
unnokid <kimki0609@gmail.com>
2024-01-01 19:23:16 +0900

    Update baekjoon BOJ27327.java

---
+++ src/baekjoon/BOJ27327.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ27327 {
+    public static void main(String[] args) throws IOException {
+        //백준 27327 Hour
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N*24);
+    }
+}
