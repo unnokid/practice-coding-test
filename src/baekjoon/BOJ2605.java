8150ee3b4750695b3d956d2d3c82f6b6e4558c65
unnokid <kimki0609@gmail.com>
2024-01-27 00:40:40 +0900

    Update baekjoon BOJ2605.java

--- src/baekjoon/BOJ2605.java
+++ src/baekjoon/BOJ2605.java
@@ -1,7 +1,9 @@
+package baekjoon;
+
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class BOJ2605 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
