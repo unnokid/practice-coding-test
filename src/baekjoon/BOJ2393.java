a6e62fec428c48da3150ca5abe12495631851ea9
unnokid <kimki0609@gmail.com>
2024-02-04 22:20:25 +0900

    Update baekjoon BOJ2393.java

---
+++ src/baekjoon/BOJ2393.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+public class BOJ2393 {
+    public static void main(String[] args) {
+        //백준 2393 Rook
+
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
