5c382075d586b7cf2a696ceffdb713f0d2d24743
Kihyun Kim <58356031+unnokid@users.noreply.github.com>
2024-03-08 17:04:38 +0900

    Update baekjoon BOJ31090.java

---
+++ src/baekjoon/BOJ31090.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //백준 31090 2023은 무엇이 특별할까?
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<N;i++){
+            int target = Integer.parseInt(br.readLine());
+            int next = target+1;
+            
+            if(next%(target%100) == 0){
+                sb.append("Good").append("\n");
+            }else{
+                sb.append("Bye").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
