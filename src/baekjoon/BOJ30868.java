e2d0cde793a8b4c61fb629e1a116ef46bf244796
Kihyun Kim <58356031+unnokid@users.noreply.github.com>
2024-03-11 17:27:40 +0900

    Update baekjoon BOJ30868.java

---
+++ src/baekjoon/BOJ30868.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //백준 30868 개표
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<N;i++){
+            int num = Integer.parseInt(br.readLine());
+            int j = num/5;
+            int n = num%5;
+            
+            for(int k =0;k<j;k++){
+                sb.append("++++ ");
+            }
+            for(int k =0;k<n;k++){
+                sb.append("|");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
