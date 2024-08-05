698e3ac6bfdaefc3805f2e1f7739460a2551cb03
unnokid <unnokid>
2024-08-05 17:00:43 +0900

    Update baekjoon BOj10821.java

---
+++ src/baekjoon/BOJ10821.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10821 {
+    public static void main(String[] args) throws IOException {
+        //백준 10821 정수의 개수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(",");
+        System.out.println(s.length);
+
+
+    }
+}
